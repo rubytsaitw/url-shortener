d8e8d8573e44713155eb8bfb6b30e48a4cde5b9b
Ruby Tsai <hjtsai0307@gmail.com>
2021-07-26 21:14:18 +0800

    feat: add index page

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const exphbs = require('express-handlebars')
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
