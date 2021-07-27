9f57c566b8d0ae339ea4b7a7e4870a0bd2f6250b
Ruby Tsai <hjtsai0307@gmail.com>
2021-07-27 15:15:54 +0800

    feat: add config/mongoose, body-parser, and method-override

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const routes = require('./routes')
+require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
-
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use(routes)
 
 app.listen(PORT, () => {
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,11 +5,39 @@ const router = express.Router()
 
 // Get home page
 router.get('/', (req, res) => {
-  const cookie = req.cookies
   res.render('index')
 })
 
 // Provide original url for shortening
+// router.post('/', (req, res) => {
+// const { inputUrl } = req.body
+// Url.create({
+//     name, image, category, rating, location, google_map, phone, description
+//   })
+//     .then(() => res.redirect('/'))
+//     .catch(error => console.log(error))
+// })
 
+
+router.post('/', (req, res) => {
+  const { inputUrl } = req.body
+  console.log('inputUrl entered:', inputUrl)
+  Url.find({ inputUrl })
+    .lean()
+    .then(url => {
+      if (url.length > 0) {
+        res.render('short', { shortUrl })
+      }
+      else if (url.length === 0) {
+        ({
+          inputUrl,
+          shortUrl: generateShortUrl()
+        })
+        console.log('new url:', url)
+        res.render('short', { shortUrl })
+      }
+    })
+    .catch(error => console.log(error))
+})
 
 module.exports = router
