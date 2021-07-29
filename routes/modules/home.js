d51eb2af5061dc4ed751cdf3847e440adde06684
Ruby Tsai <hjtsai0307@gmail.com>
2021-07-29 14:00:57 +0800

    feat: redirect short url to original url (but not working yet)

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const projectUrl = "https://very-very-short-url.herokuapp.com/"
 
 const Url = require('../../models/url')
 const generateShortUrl = require('../../tools/helpers')
@@ -16,7 +17,7 @@ router.post('/', async (req, res) => {
   const inputUrl = req.body.inputUrl.trim()
   const url = await Url.find({ inputUrl }).lean()
   if (url.length !== 0) {
-    return res.render('short', { shortUrl: url[0].shortUrl })
+    return res.render('short', { projectUrl, shortUrl: url[0].shortUrl })
   }
   // if not, generate unique short url
   const shortUrlArr = await Url.find().distinct('shortUrl').lean()
@@ -29,8 +30,21 @@ router.post('/', async (req, res) => {
     inputUrl: inputUrl,
     shortUrl: tempUrl
   })
-    .then(() => res.render('short', { shortUrl: tempUrl }))
+    .then(() => res.render('short', { projectUrl, shortUrl: tempUrl }))
     .catch(error => console.log(error))
+})
+
+// link to original url
+// not resolved
+router.get('/:id', (req, res) => {
+  console.log(req.body)
+  console.log(req.params)
+  Url.find({ shortUrl: req.params.id })
+    .lean()
+    .then(url => {
+      res.redirect(url[0].inputUrl)
+    })
+    .catch(err => console.error(err))
 })
 
 module.exports = router
