9d272f4c4234c66bad34d809ac243cd75387c7fa
Ruby Tsai <hjtsai0307@gmail.com>
2021-07-31 22:47:48 +0800

    test MONGODB_URI on heroku

--- config/mongoose.js
+++ config/mongoose.js
@@ -7,11 +7,11 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true 
 const db = mongoose.connection
 
 db.on('error', () => {
-  console.log('mongodb error')
+  console.log('mongodb error,', MONGODB_URI)
 })
 
 db.once('open', () => {
-  console.log('mongodb connected')
+  console.log('mongodb connected', MONGODB_URI)
 })
 
 module.exports = db
